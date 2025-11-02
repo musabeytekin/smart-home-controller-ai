cfa79f5ef170c3e5a1c8cfb5414a9d741ca7170b
musabeytekin <musabeytekin497@gmail.com>
2025-11-02 14:13:09 +0000

    Add lights control functionality to Door class

--- src/core/models/door.py
+++ src/core/models/door.py
@@ -1,7 +1,8 @@
 class Door:
-    def __init__(self, id: str, is_locked: bool):
+    def __init__(self, id: str, is_locked: bool, lights_on: bool = False):
         self.id = id
         self.is_locked = is_locked
+        self.lights_on = lights_on
     
     def lock(self):
         self.is_locked = True
@@ -9,3 +10,8 @@ class Door:
     def unlock(self):
         self.is_locked = False
 
+    def turn_lights_on(self):
+        self.lights_on = True
+    
+    def turn_lights_off(self):
+        self.lights_on = False
