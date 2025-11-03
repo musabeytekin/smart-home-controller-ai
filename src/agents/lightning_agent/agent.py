74b6b71406296273c1f0207ecdc93661da8ca1d9
musabeytekin <musabeytekin497@gmail.com>
2025-11-03 16:43:37 +0000

    Refactor lightning agent to use container for chat model and enhance prompt with house plan details

--- src/agents/lightning_agent/agent.py
+++ src/agents/lightning_agent/agent.py
@@ -1,18 +1,22 @@
+from smart_home_controller.src.core.container import container
 from .tools import LIGHTNING_TOOLKIT
 from langchain.agents import create_agent
-from smart_home_controller.src.core.chat_model import get_chat_model
+
 
 __LIGHTNING_AGENT_PROMPT = (
     "You are a smart home lighting assistant. "
     "Control the lighting in the house based on user requests. "
+    "House plan details:"
+    "\n ------- \n"
+    f"{container.house.get_house_plan()}"
+    "\n ------- \n"
     "Use the available tools to turn lights on or off in specific rooms or throughout the house. "
     "Always confirm the actions taken in your final response."
 )
 
 lightning_agent = create_agent(
-    model=get_chat_model(),
+    model=container.chat_model,
     tools=LIGHTNING_TOOLKIT,
     system_prompt=__LIGHTNING_AGENT_PROMPT,
 )
-
 
