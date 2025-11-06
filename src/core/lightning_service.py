41d29dd104f89e4ae9011b92fb1d6166677092a2
musabeytekin <musabeytekin497@gmail.com>
2025-11-06 21:21:23 +0000

    Enhance docstrings in LightningService methods for clarity and consistency

--- src/core/lightning_service.py
+++ src/core/lightning_service.py
@@ -1,23 +1,51 @@
 from abc import ABC, abstractmethod
+from core.exception.lightning_error import LightingError
 
 class LightningService(ABC):
 
     @abstractmethod
     def turn_on_all_lights(self):
+        """
+        Turn on all lights in the house.
+
+        :raises LightingError: If there is an issue with the lighting system.
+        """
         pass
 
     @abstractmethod
     def turn_off_all_lights(self):
+        """
+        Turn off all lights in the house.
+        :raises LightingError: If there is an issue with the lighting system.
+        """
         pass
 
     @abstractmethod
     def turn_on_room_light(self, room_id: str):
+        """
+        Turn on the light in a specific room.
+
+        :param room_id: The ID of the room.
+        :raises LightingError: If there is an issue with the lighting system.
+        """
         pass
 
     @abstractmethod
     def turn_off_room_light(self, room_id: str):
+        """
+        Turn off the light in a specific room.
+
+        :param room_id: The ID of the room.
+        :raises LightingError: If there is an issue with the lighting system.
+        """
         pass
 
     @abstractmethod
     def turn_off_lights_in_rooms(self, room_ids: list[str]):
+        """
+        Turn off the lights in specific rooms.
+
+        :param room_ids: The IDs of the rooms.
+        :raises LightingError: If there is an issue with the lighting system.
+        """
         pass
